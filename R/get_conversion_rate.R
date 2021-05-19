2fb3b0ff1f896a90a27b2fc0e60ce39245d63d33
yurialmeida5 <yurialmeida5@gmail.com>
2021-05-19 09:39:33 +0000

    Add updated get_conversion_rate

--- R/get_conversion_rate.R
+++ R/get_conversion_rate.R
@@ -22,3 +22,48 @@ get_conversion_rate <- function(retried = 0) {
   log_info('1 USD={conversion_rate} HUF')
   conversion_rate
 }
+
+
+#' Format numbers as Hungarian forints
+#' @param start_date date
+#' @param end_date date
+#' @param fromCUR base currency that you want to convert
+#' @param toCUR final currency
+#' @inheritParams get_conversion_rate
+#' @return \code{data.table} object
+#' @export
+#' @importFrom jsonlite fromJSON
+#' @importFrom checkmate assert_numeric
+#' @importFrom logger log_error log_info
+#' @importFrom data.table data.table
+#' @importFrom httr GET content
+#' @examples
+#' get_exchange_rates('EUR', 'USD', start_date = '2020-05-12', end_date = '2020-05-13')
+
+
+get_conversion_rate_month <- function(fromCUR, toCUR ,start_date = Sys.Date() - 30, end_date = Sys.Date(), retried = 0) {
+  tryCatch({
+    response <- GET(
+      'https://api.exchangerate.host/timeseries',
+      query = list(
+        start_date = start_date,
+        end_date = end_date,
+        base = fromCUR,
+        symbols = toCUR
+      )
+    )
+    #stop_for_status(response)
+    exchange_rates <- content(response)$rates
+    convrate_table <- data.table(
+      date = as.Date(names(exchange_rates)),
+      convrate = as.numeric(unlist(exchange_rates)))
+    assert_numeric(convrate_table$convrate)
+  }, error = function(e) {
+    ## str(e)
+    log_error(e$message)
+    Sys.sleep(1 + retried ^ 2)
+    get_conversion_rate_month(retried = retried + 1)
+  })
+  convrate_table
+}
+
