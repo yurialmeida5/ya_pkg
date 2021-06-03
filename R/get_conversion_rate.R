9097e2ed70c6d996cddc9c7cf3c6d5a76ea59eff
yurialmeida5 <yurialmeida5@gmail.com>
2021-06-03 18:19:57 +0000

    Add final function updates

--- R/get_conversion_rate.R
+++ R/get_conversion_rate.R
@@ -1,4 +1,4 @@
-#' Format numbers as Hungarian forints
+#' Returns the current exchange rate from USD to HUF
 #' @param retried count the number of tries
 #' @return number
 #' @export
@@ -6,10 +6,10 @@
 #' @importFrom checkmate assert_number
 #' @importFrom logger log_error log_info
 #' @examples
-#' get_conversion_rate(retried = 2)
+#' get_usdhuf(retried = 2)
 
 
-get_conversion_rate <- function(retried = 0) {
+get_usdhuf <- function(retried = 0) {
   tryCatch({
     conversion_rate <- fromJSON('https://api.exchangerate.host/latest?base=USD&symbols=HUF')$rates$HUF
     assert_number(conversion_rate, lower = 250, upper = 400)
@@ -17,31 +17,32 @@ get_conversion_rate <- function(retried = 0) {
     ## str(e)
     log_error(e$message)
     Sys.sleep(1 + retried ^ 2)
-    get_conversion_rate( retried = retried + 1)
+    get_usdhuf( retried = retried + 1)
   })
   log_info('1 USD={conversion_rate} HUF')
   conversion_rate
 }
 
 
-#' Format numbers as Hungarian forints
+#' Returns the exchange rates from one currency to another from a start to an end-date
 #' @param start_date date
 #' @param end_date date
 #' @param fromCUR base currency that you want to convert
 #' @param toCUR final currency
-#' @inheritParams get_conversion_rate
+#' @inheritParams get_usdhuf
 #' @return \code{data.table} object
 #' @export
 #' @importFrom jsonlite fromJSON
 #' @importFrom checkmate assert_numeric
-#' @importFrom logger log_error log_info
+#' @importFrom logger log_error
 #' @importFrom data.table data.table
 #' @importFrom httr GET content
 #' @examples
 #' get_exchange_rates('EUR', 'USD', start_date = '2020-05-12', end_date = '2020-05-13')
+#' get_exchange_rates('EUR', 'HUF', start_date = '2020-05-12', end_date = '2020-05-13')
 
 
-get_conversion_rate_month <- function(fromCUR, toCUR ,start_date = Sys.Date() - 30, end_date = Sys.Date(), retried = 0) {
+get_exchange_rates <- function(fromCUR, toCUR ,start_date = Sys.Date() - 30, end_date = Sys.Date(), retried = 0) {
   tryCatch({
     response <- GET(
       'https://api.exchangerate.host/timeseries',
@@ -62,7 +63,7 @@ get_conversion_rate_month <- function(fromCUR, toCUR ,start_date = Sys.Date() - 
     ## str(e)
     log_error(e$message)
     Sys.sleep(1 + retried ^ 2)
-    get_conversion_rate_month(retried = retried + 1)
+    get_exchange_rates(retried = retried + 1)
   })
   convrate_table
 }
